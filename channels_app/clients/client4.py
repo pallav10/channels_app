4fc9c112fff98796c9fcf68e3d9f49dd1cfa10bd
pallav10 <pallav8parikh@gmail.com>
2021-11-01 21:08:45 +0400

    added coroutines for parallel input and output

--- channels_app/clients/client4.py
+++ channels_app/clients/client4.py
@@ -1,8 +1,9 @@
 # Run ``pip install websockets`` before importing the library.
 # !/usr/bin/env python3
-import asyncio
+from asyncio import gather, get_event_loop
 
 import websockets
+# The client is also as an asynchronous context manager.
 from aioconsole import ainput
 
 
@@ -10,23 +11,39 @@ from aioconsole import ainput
 # The client is actually an awaiting function that yields an
 # object which can then be used to send and receive messages.
 
-# The client is also as an asynchronous context manager.
+
 async def socket_func():
     connection = websockets.connect(uri="ws://127.0.0.1:8080/ws/rearrange/4")
-    async with connection as websocket:
-        # Sends a message.
-        await websocket.send(await ainput("Enter numbers separated by spaces: "))
 
-        # Receives the replies.
-        async for message in websocket:
-            print(message)
+    async def take_input():
+        async with connection as websocket:
+            # Sends a message.
+            await websocket.send(await ainput())
+            await print_message(take_new_input=True)
 
+    async def print_message(take_new_input=False):
+        async with connection as websocket:
+            # Sends a message.
+
+            # Receives the replies.
+            if not take_new_input:
+                async for message in websocket:
+                    print(message)
+            if take_new_input:
+                await take_input()
+
+    async def multiple():
+        tasks = [take_input(), print_message()]
+        await gather(*tasks)
         # Closes the connection.
-        await websocket.close()
+        # await websocket.close()
+
+    while True:
+        await multiple()
 
 
 def main():
-    loop = asyncio.get_event_loop()
+    loop = get_event_loop()
     loop.run_until_complete(socket_func())
     loop.close()
 
